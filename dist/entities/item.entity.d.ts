6f5f4736b0a7cd6822043ee2623dec026c824723
taroosg <macchinetta.xyz@gmail.com>
2020-07-16 14:48:40 +0900

    update config file

--- dist/entities/item.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Item {
-    readonly id: number;
-    todo: string;
-    limit: Date;
-    isDone: boolean;
-    deletePassword: string;
-    readonly createdAt?: Date;
-    readonly updatedAt?: Date;
-}
