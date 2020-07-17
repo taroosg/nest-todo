5c19a870435891f545dd2574a8b94841f74999ec
taroosg <macchinetta.xyz@gmail.com>
2020-07-18 00:32:12 +0900

    update config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { TypeOrmModule } from "@nestjs/typeorm"; //追加！
-import { ItemModule } from './item/item.module';
+import { ItemModule } from "./item/item.module";
+import { Item } from "./entities/item.entity";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), ItemModule], //追加！
+  imports: [
+    ItemModule,
+    TypeOrmModule.forRoot({
+      "type": "sqlite",
+      "database": "data/dev.sqlite",
+      "entities": [
+        Item,
+      ],
+      "migrations": [__dirname + "/migrations/**/*{.ts,.js}"],
+      "logging": true,
+    }),
+  ], //追加！
   controllers: [AppController],
   providers: [AppService],
 })
